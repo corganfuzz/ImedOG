e6bdd13efad779889f7d9a1f6acb83d0608eedec
Billy Corgan <corganfuzz@gmail.com>
2016-09-06 13:25:40 -0500

    assignment 2

---
+++ app/projectz/assignment02-files/hello4.js
@@ -0,0 +1,11 @@
+// INSTRUCTIONS:
+//
+//   Using external JavaScript
+//
+// 1. Create an external JavaScript document named "hello4.js"
+// 2. Create a variable named "hello" and assign it the value "Hello JavaScript!"
+// 3. Create an alert() box that will display: "Hello JavaScript!" using the variable
+// 4. Place a link to the external JavaScript file between the <body></body> tags of the document after the comment "Insert code here"
+
+var hello = "Hello JavaScript";
+alert(hello)
